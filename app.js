c29677ebec7924ce8194bb2a06781a9be9e5603f
FauzanKhan <fauzankhan93@gmail.com>
2015-04-21 01:09:21 +0530

    Fixed 404 error

--- app.js
+++ app.js
@@ -18,11 +18,8 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
+
+// error handlers
 
 // development error handler
 // will print stacktrace
